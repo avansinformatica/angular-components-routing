7ebfa763f557c7fdfea260cdfc328973330dba66
Robin Schellius <r.schellius@avans.nl>
2021-12-05 16:24:12 +0100

    Cypress e2e/integrationtest added

--- src/app/pages/entity/user.service.spec.ts
+++ src/app/pages/entity/user.service.spec.ts
@@ -1,12 +1,19 @@
 import { TestBed } from '@angular/core/testing';
 import { UserService } from './user.service';
+import { HttpClient } from '@angular/common/http';
 
 describe('UserService', () => {
   let service: UserService;
+  let httpSpy: jasmine.SpyObj<HttpClient>;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    httpSpy = jasmine.createSpyObj('HttpClient', ['get', 'post']);
+
+    TestBed.configureTestingModule({
+      providers: [{ provide: HttpClient, useValue: httpSpy }],
+    });
     service = TestBed.inject(UserService);
+    httpSpy = TestBed.inject(HttpClient) as jasmine.SpyObj<HttpClient>;
   });
 
   it('has to be created', () => {
